ec2b2940acc3e2373718e9076203dd33bf5c913c
Soumipaul08 <paulsoumi330@gmail.com>
2025-05-25 15:47:52 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  assetsInclude:['**/*.glb']
+  assetsInclude:['**/*.glb'],
+  base:"/3D-Portfolio/"
 })
